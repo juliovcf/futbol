661274fba754986718822f634160169ce2f02a60
juliovcf <julk.perezvcf@gmail.com>
2023-01-28 17:31:59 +0100

    añadido deleteTeam

--- src/main/java/com/torneo/futbol/controller/TeamController.java
+++ src/main/java/com/torneo/futbol/controller/TeamController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import com.torneo.futbol.model.Team;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,22 +13,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 @Api(value = "Team Management System")
 @RequestMapping("/teams")
 public interface TeamController {
 
-    @ApiOperation(value = "View a list of available teams", response = List.class)
     @GetMapping
     List<Team> getAllTeams();
 
     @GetMapping(value = "/{id}")
     Optional<Team> get(@PathVariable(required = true) Long id);
 
-    @ApiOperation(value = "Add a team")
     @PostMapping
-    void addTeam(@ApiParam(value = "Team object store in database table", required = true) @RequestBody Team team);
+    void addTeam(@ApiParam(required = true) @RequestBody Team team);
+
+    @DeleteMapping("/{id}")
+    void deleteTeam(@ApiParam(required = true) @PathVariable(value = "id") Long id);
 
 }
